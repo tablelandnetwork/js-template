431b14f4b3e7723a05604c2082707518a8226259
Joe Wagner <njwjs722@gmail.com>
2023-02-06 10:24:18 -0700

    wip update tests for new tableland package version

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,31 +1,50 @@
 import { strictEqual, deepStrictEqual } from "assert";
 import { describe, test } from "mocha";
-import { getAccounts, getConnection } from "@tableland/local";
+import { getAccounts, getDatabase, getValidator } from "@tableland/local";
 
 describe("index", function () {
   // Note that we're using the second account here
   const [, signer] = getAccounts();
-  const sdk = getConnection(signer);
+  const db = getDatabase(signer);
+  const validator = getValidator();
 
   test("create", async function () {
-    const { name } = await sdk.create("counter integer", { prefix: "table" });
-    strictEqual(name, "table_31337_2");
+    const { meta } = await db
+      .prepare("CREATE TABLE my_table (counter integer);")
+      .all();
+    strictEqual(meta.txn?.name, "my_table_31337_2");
   });
 
   test("insert", async function () {
-    const { hash } = await sdk.write("insert into table_31337_2 values (1);");
-    const txnReceipt = await sdk.receipt(hash);
+    const { meta } = await db
+      .prepare("insert into my_table_31337_2 values (1);")
+      .all();
+
+    strictEqual(typeof meta.txn?.transactionHash, "string");
+    strictEqual(typeof meta.txn?.chainId, "number");
+
+    const txnReceipt = await validator.receiptByTransactionHash({
+      transactionHash: meta.txn?.transactionHash,
+      chainId: meta.txn?.chainId,
+    });
     strictEqual(txnReceipt?.chainId, 31337);
   });
 
   test("update", async function () {
-    const { hash } = await sdk.write("update table_31337_2 set counter=2;");
-    const txnReceipt = await sdk.receipt(hash);
+    const { meta } = await db
+      .prepare("update my_table_31337_2 set counter=2;")
+      .all();
+    const txnReceipt = await validator.receiptByTransactionHash({
+      transactionHash: meta.txn?.transactionHash as string,
+      chainId: meta.txn?.chainId as number,
+    });
     strictEqual(txnReceipt?.chainId, 31337);
   });
 
   test("query", async function () {
-    const { rows } = await sdk.read("select * from table_31337_2;");
-    deepStrictEqual(rows[0], [2]);
+    const { results } = await db
+      .prepare("select * from my_table_31337_2;")
+      .all();
+    deepStrictEqual(results, [{ counter: 2 }]);
   });
 });
